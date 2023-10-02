6de490d345f8e5eb966acc7b1f1a47edba7bbd3c
TCloudvn.com <TCloudvn.com@TCLOUDVN-PC>
2023-10-02 13:25:57 +0700

    'add_bai_tap

--- abc/Array.cs
+++ abc/Array.cs
@@ -78,7 +78,237 @@ namespace abc
             //     Console.WriteLine($"Tong cac gia tri trong mang la : {mssg}");
             // }
             #endregion
+            #region Bài 3 : 
+            //    int[] a = input();           
+            //}
+            //static int[] input()
+            //{
+            //    int n = int.Parse(Console.ReadLine());
+            //    int[] array= new int[n];
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        array[i] = int.Parse(Console.ReadLine());
+            //        PrimeNumb(array);
+            //    }
+            //    return array;
+            //}
+            //static void PrimeNumb(int[] d)
+            //{
+            //    for (int i = 0; i < d.Length; i++)
+            //    {
+            //        if (isPrimeNumb(d[i]))
+            //        {
+            //            Console.WriteLine((i, d[i]));
+            //        }
+            //    }
+            //}
+            //static bool isPrimeNumb(int c)
+            //{
+            //    if (c < 2)
+            //    {
+            //        return false;
+            //    }
+            //    for (int i = 2; i < Math.Sqrt(c); i++)
+            //    {
+            //        if (c % i == 0)
+            //        {
+            //            return false;
+            //        }
+            //    }
+            //    return true;
+            //}
+            #endregion
+            #region Bài 4 :
+            //    int[] a = input();
+            //    int b = SumEvenNumb(a);
+            //    Console.Write($"Trung binh cong cac so chan trong mang la : {b} ");
+            //    Console.WriteLine();
+            //}
+            //static int[] input()
+            //{
+            //    Console.Write("Nhap so luong phan tu : ");
+            //    int n = int.Parse(Console.ReadLine());
+            //    int[] array = new int[n];
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        Console.Write($"Nhap phan tu {i} : ");
+            //        array[i] = int.Parse(Console.ReadLine());
+            //        SumEvenNumb(array);
+            //    }
+            //    return array;
+            //}
+            //static int SumEvenNumb(int[] array)
+            //{
+            //    int sum = 0;
+            //    int count = 0;
+            //    for ( int i = 0; i < array.Length; i++ )
+            //    {
+            //        if (isEvenNumb(array[i]))
+            //        {
+            //            sum += array[i];
+            //            count++;
+            //        }
+            //    }
+            //    return sum / count;
+            //}
+            //static bool isEvenNumb(int c)
+            //{
+            //    if (c % 2 == 1)
+            //    {
+            //        return false;
+            //    }
+            //    return true;
+            //}
+            #endregion
+            #region Bài 5 :
+            //    int[] a = input();
+            //}
+            //static int[] input()
+            //{
+            //    Console.Write("Nhap gioi han cua mang : ");
+            //    int n = int.Parse(Console.ReadLine());
+            //    int[] array = new int[n];
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        Console.Write($"Nhap phan tu {i} : ");
+            //        array[i] = int.Parse(Console.ReadLine());
+            //        if (isSoChinhPhuong(array[i]))
+            //        {
+            //            Console.WriteLine($"Tai vi tri {i} co phan tu {array[i]} la so chinh phuong.");
+            //        }
+            //    }
+            //    return array;
+            //}
+            //static bool isSoChinhPhuong(double array)
+            //{
+            //    double d = Math.Sqrt(array);
+            //    if (d * d != array)
+            //    {
+            //        return false;
+            //    }
+            //    return true;
+            //}
+            #endregion
+            #region Bài 6 :
+            //    int[] b = input();
+            //    int a = SoThuanNghich(b);
+            //    Mssg(a);
+            //}
+            //static int[] input()
+            //{
+            //    Console.Write("Nhap gioi han cua mang : ");
+            //    int n = int.Parse(Console.ReadLine());
+            //    int[] array = new int[n];
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        Console.Write($"Nhap phan tu {i} : ");
+            //        array[i] = int.Parse(Console.ReadLine());              
+            //    }
+            //    return array;
+            //}
+            //static int SoThuanNghich(int[] array)
+            //{
+            //    int soThuanNgich = 0;
+            //    for (int i = 0; i < array.Length; i++)
+            //    {
+            //        if (isSoThuanNghic(array[i]))
+            //        {
+            //            soThuanNgich = array[i];
+            //        }
+            //    }  
+            //    return soThuanNgich;
+            //}
+            //static bool isSoThuanNghic(int n)
+            //{
+            //    int a = n;
+            //    int b = 0;
+            //    if (n < 10)
+            //    {
+            //        return false;
+            //    }
+            //    while (a > 0)
+            //    {
+            //        b = b * 10 + a % 10;
+            //        a /= 10;
+            //    }
+            //    return b == n;
+            //}
+            //static void Mssg(int msg)
+            //{
+            //    Console.Write($"So thuan nghich trong mang la : {msg} ");
+            //    Console.WriteLine();
+            //}
+            #endregion
+            #region Bài 7 :
+            int[] a = input();
+            int b = max(a);
+            int c = min(a);
+            if (Check(a))
+            {
+                mssg(b, c);
+            }
+            else
+            {
+                Console.WriteLine("Khong co gia tri max or min");
+            }
         }
+        static int[] input()
+        {
+            Console.Write("nhap gioi han cua mang : ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"nhap phan tu {i} : ");
+                array[i] = int.Parse(Console.ReadLine());
+            }                     
+            max(array);
+            min(array);                           
+            return array;
+        }
+        static int max(int[] array)
+        {
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                   max = array[i];
+                }
+            }          
+            return max;
+        }
+        static int min(int[] array)
+        {
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+        static void mssg(int msgMax, int msgMin)
+        {
+            Console.Write($"gia tri lon nhat trong mang la : {msgMax}");
+            Console.WriteLine();
+            Console.Write($"Gia tri nho nhat cua mang la : {msgMin}");
+            Console.WriteLine();
+        }
+        static bool Check(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[0] != array[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        #endregion
     }
     
 }
