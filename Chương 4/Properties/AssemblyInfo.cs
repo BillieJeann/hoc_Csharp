ffaf93d9ab73f8746ccfd964ec30536d0e0ebc66
QUANG HUY <QUANG HUY@MSI>
2023-10-16 11:09:42 +0700

    Fix Git

--- Chương 4/Properties/AssemblyInfo.cs
+++ Chương 4/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("abc")]
+[assembly: AssemblyTitle("Chương 4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("abc")]
+[assembly: AssemblyProduct("Chương 4")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c365afc1-ec0a-43d5-8b40-6cf04effea4f")]
+[assembly: Guid("6f401fac-2c4c-4e00-a829-418bd1538634")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Chương 6/binarysearch.cs
+++ Chương 6/binarysearch.cs
@@ -237,7 +237,7 @@ namespace abc
             //    return true;
             //}
             #endregion
-                
+                 
         }
     }
 }
